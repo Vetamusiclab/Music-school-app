9e448017664c0847ff044395d71b47ddf7a667f2
Vetamusiclab <vetagulliver@gmail.com>
2025-07-22 23:02:36 +0300

    Add files via upload

---
+++ Components/Footer.tsx
@@ -0,0 +1,3 @@
+export function Footer() {
+  return <footer className="p-4 bg-[#FFE0B2] text-center text-sm">© 2025 MUSIC.LAB</footer>;
+}
---
+++ Components/Header.tsx
@@ -0,0 +1,3 @@
+export function Header() {
+  return <header className="p-4 bg-[#FF6F00] text-white text-center">MUSIC.LAB</header>;
+}
