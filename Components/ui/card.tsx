fa327036d8ec40c07102c0d392db21aa57b6318d
Vetamusiclab <vetagulliver@gmail.com>
2025-07-28 10:50:48 +0300

    Update card.tsx

--- Components/ui/card.tsx
+++ Components/ui/card.tsx
@@ -1,1 +1,10 @@
+import * as React from "react"
+
+export function Card({ children }: { children: React.ReactNode }) {
+  return <div className="border rounded-2xl shadow p-4 bg-white">{children}</div>
+}
+
+export function CardContent({ children }: { children: React.ReactNode }) {
+  return <div className="p-2">{children}</div>
+}
 
