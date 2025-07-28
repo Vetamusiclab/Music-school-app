e9a70fb8667ff0de5515aabbe89c62447bff6365
Vetamusiclab <vetagulliver@gmail.com>
2025-07-28 10:50:14 +0300

    Update button.tsx

--- Components/ui/button.tsx
+++ Components/ui/button.tsx
@@ -1,1 +1,18 @@
+```tsx
+import * as React from "react"
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+children: React.ReactNode
+}
+
+export function Button({ children, ...props }: ButtonProps) {
+return (
+ <button
+   className="px-4 py-2 bg-orange-600 hover:bg-orange-700 text-white rounded-2xl transition"
+   {...props}
+ >
+   {children}
+ </button>
+)
+}
 
