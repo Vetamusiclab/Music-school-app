5ccf127ccc93b78b780785af15e0d74e258fa514
Vetamusiclab <vetagulliver@gmail.com>
2025-07-28 10:51:19 +0300

    Update input.tsx

--- Components/ui/input.tsx
+++ Components/ui/input.tsx
@@ -1,1 +1,16 @@
+import * as React from "react"
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  return (
+    <input
+      ref={ref}
+      className="border rounded px-3 py-2 w-full"
+      {...props}
+    />
+  )
+})
+
+Input.displayName = "Input"
 
