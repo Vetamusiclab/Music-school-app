111396ba715789ecb14302c08e6019bb6475ff00
Vetamusiclab <vetagulliver@gmail.com>
2025-07-28 23:37:28 +0300

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,12 +1,4 @@
-import React from "react";
-
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-export function Input(props: InputProps) {
-  return (
-    <input
-      {...props}
-      className="w-full rounded border px-3 py-2 focus:outline-none focus:ring"
-    />
-  );
+// components/ui/input.tsx
+export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return <input {...props} />
 }
