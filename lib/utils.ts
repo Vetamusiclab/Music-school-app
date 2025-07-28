fb2153ad940598a03d904e777e0c5b37da4f576a
Vetamusiclab <vetagulliver@gmail.com>
2025-07-28 11:16:48 +0300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,9 @@
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-variants"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+type ClassValue = string | number | null | boolean | undefined | ClassValue[]
 
